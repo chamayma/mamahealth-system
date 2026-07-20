be35d3a114dbd5f50e0d8a56d5fb0664d8a34001
chamayma <chamayma22@gmail.com>
2026-07-20 12:51:29 +0300

    Configure GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/mamahealth-system/',
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:8080',
+        target: 'https://mamahealth-backend.onrender.com',
         changeOrigin: true,
         secure: false,
       }
